12959c7a2f4e9acfdffdd1b163d756567e113916
SamAddy <addisonsamuel70@yahoo.com>
2023-06-16 16:35:28 +0300

    Do more testing

--- src/Program.cs
+++ src/Program.cs
@@ -14,14 +14,17 @@ internal class Program
         Book book2 = new Novel("Novel 1", "Author 2", "9876543210", "2021", "B002", "Thriller");
         Customer customer1 = new Customer("John", "Smith", "john.smith@mail.com", "12345pass", "customer-1");
         Customer customer2 = new Customer("Steve", "Huhhs", "steve.huhhs@mail.com", "password1", "customer-2");
-
-        library.AddBook(book1);
-        library.AddBook(book2);
+        Librarian librarian = new Librarian("Mark", "Anthony", "mark.anthony@mail.com", "lib123", "librarian-1");
+    
+        library.AddBook(book1, librarian.Id);
+        library.AddBook(book2, librarian.Id);
         library.AddPerson(customer1);
         library.AddPerson(customer2);
+        library.AddPerson(librarian);
+        library.RemoveCustomer(customer2.Id);
 
-        library.BorrowBook("B001", customer1);
-        library.BorrowBook("B0029", customer2);
+        library.BorrowBook("B001", customer1.Id);
+        library.BorrowBook("B0029", customer2.Id);
         Console.WriteLine();
         library.PrintBooks();
     }
